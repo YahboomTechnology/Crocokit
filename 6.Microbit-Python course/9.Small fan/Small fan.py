a78e7b5f13d81dff654464edfc4cc287af95791e
zhang <2448532184@qq.com>
2020-07-17 11:58:24 +0800

    update fan pattern

--- 6.Microbit-Python course/9.Small fan/Small fan.py
+++ 6.Microbit-Python course/9.Small fan/Small fan.py
@@ -1,12 +1,13 @@
 from microbit import *
-fun1 = Image("00990:90900:99999:00909:09900")
-fun2 = Image("99009:09099:00900:99090:90099")
-all_fun = [fun1, fun2]
+
 while True:
     if button_a.is_pressed():
         while True:
             pin0.write_digital(1)
-            display.show(all_fun, delay=100)
+            display.show(Image.HEART)
+            sleep(1000)
+            display.show(Image.HEART_SMALL)
+            sleep(1000)
     else:
-        display.show(Image.HEART)
+        display.show(Image.HAPPY)
         display.clear()
